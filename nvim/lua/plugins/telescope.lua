22bd33f3dbbdaa72d1e8754c5a4c7662f3cd9fc5
Osa <osalorenzo@gmail.com>
2025-10-15 01:43:26 -0400

    Update telescope.lua

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -4,6 +4,7 @@ return {
 	config = function()
 		local builtin = require("telescope.builtin")
 		vim.keymap.set("n", "<leader>ff", builtin.find_files, { noremap = true, silent = true })
+		vim.keymap.set("n", "<leader><space>", builtin.find_files, { noremap = true, silent = true })
 		vim.keymap.set("n", "<leader>fg", builtin.live_grep, { noremap = true, silent = true })
 	end,
 }
